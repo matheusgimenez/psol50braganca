c6161f35f574dc478231a1ee0d4459240504d3ce
rodica-andronache <rodica.andronache@vertistudio.com>
2014-10-09 11:55:18 +0300

    Fixed customizer syntax error

--- includes/customizer.php
+++ includes/customizer.php
@@ -22,8 +22,8 @@ function lawyeria_lite_customizer( $wp_customize ) {
 	/* theme notes */
 	$wp_customize->add_section( 'lawyeria_lite_theme_notes' , array(
 		'title'      => __('ThemeIsle theme notes','lawyeria-lite'),
-		'description' => sprintf( __( "Thank you for being part of this! We've spent almost 6 months building ThemeIsle without really knowing if anyone will ever use a theme or not, so we're very grateful that you've decided to work with us. Wanna <a href='http://themeisle.com/contact/' target='_blank'>say hi</a>?
-		<br/><br/><a href='http://themeisle.com/demo/?theme=Lawyeria%20Lite' target='_blank' />View Theme Demo</a> | <a href='http://themeisle.com/forums/forum/lawyeria-lite' target='_blank'>Get theme support</a><br/><br/><a href='http://themeisle.com/documentation-lawyeria-lite' target='_blank'>Documentation</a><br><br><a href='https://themeisle.com/themes/lawyeria-attorney-lawyer-wordpress-theme/' target='_blank' style='color:red'>Upgrade to PRO</a> ",'lawyeria-lite')),
+		'description' => __( 'Thank you for being part of this! We\'ve spent almost 6 months building ThemeIsle without really knowing if anyone will ever use a theme or not, so we\'re very grateful that you\'ve decided to work with us. Wanna <a href="http://themeisle.com/contact/" target="_blank">say hi</a>?
+		<br/><br/><a href="http://themeisle.com/demo/?theme=Lawyeria%20Lite" target="_blank" />View Theme Demo</a> | <a href="http://themeisle.com/forums/forum/lawyeria-lite" target="_blank">Get theme support</a><br/><br/><a href="http://themeisle.com/documentation-lawyeria-lite" target="_blank">Documentation</a><br><br><a href="https://themeisle.com/themes/lawyeria-attorney-lawyer-wordpress-theme/" target="_blank" style="color:red;">Upgrade to PRO</a> ','lawyeria-lite'),
 		'priority'   => 30,
 	));
 	$wp_customize->add_setting(
