1a5de4691e016af944e1e29eac2bc7dbd9e2f254
rodica-andronache <rodik_andronache@yahoo.com>
2015-04-06 12:48:35 +0300

    Fixed #3, link in footer, powered by wordpress

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 						}
 					?>
 				</div><!--/div .footer-margin-left .cf-->
-
+				<?php echo '<div class="lawyeria_lite_poweredby"><a class="" href="https://themeisle.com/themes/lawyeria-lite/" target="_blank" rel="nofollow">Lawyeria Lite </a>'.__('powered by','lawyeria-lite').'<a class="" href="http://wordpress.org/" target="_blank" rel="nofollow"> WordPress</a></div>'; ?>
 			</div><!--/div .wrapper .cf-->
 		</footer><!--/footer #footer-->
 		<?php wp_footer(); ?>
